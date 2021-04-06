34f09ab9949fac72be27a9a28ecbe1cf061c50a2
Donivan Hughes <donivan.hughes@workhorse.com>
2021-04-06 10:34:41 -0400

    Added SafeDisplay function

--- UExtention/Objects.cs
+++ UExtention/Objects.cs
@@ -1,18 +1,36 @@
 ﻿
 
 using System;
+using UExtensionLibrary.Reflection;
 
 namespace UExtensionLibrary.Objects
 {
     public static class Objects
     {
-
+        /// <summary>
+        /// Performs check to see if object is DBNull, and if so changes it to .Net null
+        /// </summary>
+        /// <param name="objBase">Object to check</param>
+        /// <returns>If object is DBNull, returns null, else returns object</returns>
         public static object SafeValue(this object objBase)
         {
 
             if (DBNull.Value.Equals(objBase)) objBase = null;
             return objBase;
         }
-        
+        /// <summary>
+        /// Checks if object is null, then looks for the property.
+        /// </summary>
+        /// <param name="CheckObject">Object to check for null</param>
+        /// <param name="CheckObjectProperty">Property to get value from</param>
+        /// <param name="NullResultString">String to return if @CheckObject is null</param>
+        /// <returns>If Object is null, returns @NullResultString, else returns specified properties value as string</returns>
+        public static string SafeDisplay(this object CheckObject, string CheckObjectProperty, string NullResultString)
+        {
+            string ReturnString = NullResultString;
+            if (CheckObject != null) ReturnString = CheckObject.GetPropertyValue(CheckObjectProperty).ToString();
+            return ReturnString;
+        }
+
     }
 }
--- UExtention/Properties/AssemblyInfo.cs
+++ UExtention/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("UExtentionLibrary")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library of useful functions")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("CIProjects")]
+[assembly: AssemblyCompany("Donivan Hughes")]
 [assembly: AssemblyProduct("UExtentionLibrary")]
-[assembly: AssemblyCopyright("Copyright © Donivan Hughes 2016")]
+[assembly: AssemblyCopyright("Copyright © Donivan Hughes 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.1.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: AssemblyVersion("1.1.1.1")]
+[assembly: AssemblyFileVersion("1.1.1.1")]
