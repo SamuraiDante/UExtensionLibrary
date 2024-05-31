9678f8651c04c3af6f3db5909eaa30866400c604
SamuraiDante <82036972+SamuraiDante@users.noreply.github.com>
2024-05-31 11:33:08 -0400

    Added Tests

---
+++ TestingApplication/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using UExtensionLibrary.Extensions;
+using UExtensionLibrary.Extensions.Test;
+
+
+ReflectionTest.RunTests();
+
--- UExtention/Reflection.cs
+++ UExtention/Reflection.cs
@@ -1,172 +1,146 @@
-﻿// // -------------------------------------------------------------------------
-// // File: Enumerables.cs
-// // Author: Hughes, Donivan(hughes.dh.1)
-// // Abstract: TODO: Add abstract
-// //
-// // Created: 07/12/2016 11:11 AM
-// // Last Updated: 07/12/2016 11:11 AM
-// // -------------------------------------------------------------------------
-// 
-// // -------------------------------------------------------------------------
-// // Imports
-// // -------------------------------------------------------------------------
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Data;
-using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Web.Script.Serialization;
-using System.Windows.Forms.VisualStyles;
 
 namespace UExtensionLibrary.Extensions
 {
-    /// <summary>
-    /// Class to assist in using the reflection library
-    /// </summary>
     public static class Reflection
     {
-
-        /// ------------------------------------------------------------------------------------------
-        /// Name: GetPropertyValue
-        /// ------------------------------------------------------------------------------------------
-        /// <summary> Gets the specified properties value. </summary>
+        /// <summary>
+        /// Gets the specified property's value.
+        /// </summary>
         /// <param name="objObject">The containing object.</param>
-        /// <param name="strPropertyName">The name of the property.</param>
-        /// <returns>System.Object.</returns>
-        /// ------------------------------------------------------------------------------------------
-        public static object GetPropertyValue(this object objObject, string strPropertyName)
+        /// <param name="propertyName">The name of the property.</param>
+        /// <returns>The property's value or a message indicating failure.</returns>
+        public static object GetPropertyValue(this object objObject, string propertyName)
         {
-            //Get the property type
-            Type objType = objObject.GetType();
-            object objValue = null;
-            PropertyInfo pInfo = default(PropertyInfo);
-            string strPropertyValue = null;
+            if (objObject == null || string.IsNullOrEmpty(propertyName))
+            {
+                throw new ArgumentNullException(nameof(objObject), "Object or property name cannot be null.");
+            }
 
-            //Attempt to get the properties info
             try
             {
-                pInfo = objType.GetProperty(strPropertyName);
+                PropertyInfo propertyInfo = objObject.GetType().GetProperty(propertyName);
+                if (propertyInfo == null)
+                {
+                    return "Property not found";
+                }
+
+                object value = propertyInfo.GetValue(objObject);
+                return GetValueAsString(value);
             }
             catch (Exception ex)
             {
-                //If unable to get the info, return a string stating that as the value
-                strPropertyValue = "Unable to determine value";
-                return strPropertyValue;
+                return $"Unable to determine value: {ex.Message}";
             }
-
-            IEnumerable ienuValue = default(IEnumerable);
-
-            //Ignore setter, as they contain no value
-            if (!strPropertyName.Contains("set_") && pInfo != null)
-            {
-                //Remove the gets from getters, so it is formatted correctly for GetValue
-                strPropertyName = strPropertyName.Replace("get_", "");
-                try
-                {
-                    //Get the value
-                    objValue = pInfo.GetValue(objObject, null);
-
-                    //If the value is enummerable, build a single string from the enummerables values
-                    ienuValue = objValue as IEnumerable;
-
-                    //Value will be nothing if not enumberable
-                    if ((objValue) is string || (ienuValue == null))
-                    {
-                        strPropertyValue = objValue.ToString();
-                    }
-                    else
-                    {
-                        IEnumerable itemLoopVariables = ienuValue as object[] ?? ienuValue.Cast<object>().ToArray();
-                        if (!itemLoopVariables.IsNullOrEmpty())
-                        {
-                            strPropertyValue += '\n';
-                            foreach (object Item_loopVariable in itemLoopVariables)
-                            {
-                                object Item = Item_loopVariable;
-                                strPropertyValue += "\t\t" + Item + '\t';
-                            }
-                        }
-                        else
-                        {
-                            //If the enummerable has no values, send that information back as the value
-                            strPropertyValue = "Empty";
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    //If anything breaks, give up and move on
-                    strPropertyValue = "Unable to determine value";
-                }
-
-            }
-
-            return strPropertyValue;
         }
 
-        /// ------------------------------------------------------------------------------------------
-        /// Name: GetFieldValue
-        /// ------------------------------------------------------------------------------------------
-        /// <summary> Gets the specified fields value. </summary>
+        /// <summary>
+        /// Gets the specified field's value.
+        /// </summary>
         /// <param name="objObject">The containing object.</param>
-        /// <param name="strFieldName">The name of the field.</param>
-        /// <returns>System.Object.</returns>
-        /// ------------------------------------------------------------------------------------------
-        public static object GetFieldValue(object objObject, string strFieldName)
+        /// <param name="fieldName">The name of the field.</param>
+        /// <returns>The field's value or a message indicating failure.</returns>
+        public static object GetFieldValue(this object objObject, string fieldName)
         {
-            Type objType = objObject.GetType();
-            object objValue = null;
-
-            FieldInfo fInfo = objType.GetField(strFieldName);
-            IEnumerable ienuValue = default(IEnumerable);
-            object strFieldValue = null;
-            if (fInfo != null)
+            if (objObject == null || string.IsNullOrEmpty(fieldName))
             {
-                try
-                {
-                    //Get value as object
-                    objValue = fInfo.GetValue(objObject);
-
-                    //check if the value is an enumerable list
-                    ienuValue = objValue as IEnumerable;
-
-                    //Value will be nothing if not enumberable
-                    if ((objValue) is string || ienuValue == null)
-                    {
-                        strFieldValue = objValue;
-                    }
-                    else
-                    {
-                        IEnumerable itemLoopVariables = ienuValue as object[] ?? ienuValue.Cast<object>().ToArray();
-                        if (!itemLoopVariables.IsNullOrEmpty())
-                        {
-                            foreach (object Item_loopVariable in itemLoopVariables)
-                            {
-                                object Item = Item_loopVariable;
-                                strFieldValue += "\t\t" + Item + '\t';
-                            }
-                        }
-                        else
-                        {
-                            strFieldValue = "Empty";
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    strFieldValue = "Unable to determine value";
-                }
-
+                throw new ArgumentNullException(nameof(objObject), "Object or field name cannot be null.");
             }
 
-            return strFieldValue;
+            try
+            {
+                FieldInfo fieldInfo = objObject.GetType().GetField(fieldName);
+                if (fieldInfo == null)
+                {
+                    return "Field not found";
+                }
+
+                object value = fieldInfo.GetValue(objObject);
+                return GetValueAsString(value);
+            }
+            catch (Exception ex)
+            {
+                return $"Unable to determine value: {ex.Message}";
+            }
         }
 
+        /// <summary>
+        /// Converts an object value to a string representation.
+        /// </summary>
+        /// <param name="value">The object value.</param>
+        /// <returns>A string representation of the value.</returns>
+        private static string GetValueAsString(object value)
+        {
+            if (value == null)
+            {
+                return "Null";
+            }
 
+            if (value is string strValue)
+            {
+                return strValue;
+            }
+
+            if (value is IEnumerable enumerableValue && !(value is string))
+            {
+                var items = enumerableValue.Cast<object>().Select(item => item?.ToString() ?? "Null").ToArray();
+                return items.Any() ? string.Join("\t\t", items) : "Empty";
+            }
+
+            return value.ToString();
+        }
+    }
+}
+
+namespace UExtensionLibrary.Extensions.Test
+{
+    public class ReflectionTest
+    {
+        public enum TestEnum
+        {
+            First,
+            Second,
+            Third
+        }
+
+        public class TestClass
+        {
+            public string StringProperty { get; set; } = "Test String";
+            public int IntProperty { get; set; } = 42;
+            public List<string> ListProperty { get; set; } = new List<string> { "One", "Two", "Three" };
+            public string NullProperty { get; set; } = null;
+            public TestEnum EnumProperty { get; set; } = TestEnum.Second;
+            public string StringField = "Test Field String";
+            public int IntField = 84;
+            public List<string> ListField = new List<string> { "Four", "Five", "Six" };
+            public string NullField = null;
+            public TestEnum EnumField = TestEnum.Third;
+        }
+
+        public static void RunTests()
+        {
+            var testObject = new TestClass();
+
+            // Test GetPropertyValue
+            Console.WriteLine(testObject.GetPropertyValue("StringProperty")); // Expected: "Test String"
+            Console.WriteLine(testObject.GetPropertyValue("IntProperty")); // Expected: "42"
+            Console.WriteLine(testObject.GetPropertyValue("ListProperty")); // Expected: "One\t\tTwo\t\tThree"
+            Console.WriteLine(testObject.GetPropertyValue("NullProperty")); // Expected: "Null"
+            Console.WriteLine(testObject.GetPropertyValue("EnumProperty")); // Expected: "Second"
+            Console.WriteLine(testObject.GetPropertyValue("NonExistentProperty")); // Expected: "Property not found"
+
+            // Test GetFieldValue
+            Console.WriteLine(Reflection.GetFieldValue(testObject, "StringField")); // Expected: "Test Field String"
+            Console.WriteLine(Reflection.GetFieldValue(testObject, "IntField")); // Expected: "84"
+            Console.WriteLine(Reflection.GetFieldValue(testObject, "ListField")); // Expected: "Four\t\tFive\t\tSix"
+            Console.WriteLine(Reflection.GetFieldValue(testObject, "NullField")); // Expected: "Null"
+            Console.WriteLine(Reflection.GetFieldValue(testObject, "EnumField")); // Expected: "Third"
+            Console.WriteLine(Reflection.GetFieldValue(testObject, "NonExistentField")); // Expected: "Field not found"
+        }
 
     }
-
 }
